79da5ea8b14f3d215bc4138f617ea3a68d6476b1
Tera Coder <TerableCoder@gmail.com>
2019-01-27 21:27:46 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,65 @@
+module.exports = {
+    enable: true,
+    enableAuto: true,
+	lootDelay: 30,
+    loopInterval: 30,
+
+    blacklist: [
+        7214, // Scroll of Resurrection
+        8000, // Rejuvenation Mote
+        8001, // HP Recovery Mote
+        8002, // MP Replenishment Mote
+        8003, // Spirited MP Replenishment Mote
+        8004, // Strong Resistance Mote
+        8005, // Healing Mote
+        8008, 8009, 8010, 8011, 8012, 8013, 8014, 8015, 8016, 8017, 8018, 8019, 8020, 8021, 8022, // Arun's Vitae I-XV Mote
+        8023, // Arun's Tear Mote
+        8025, // Keening Dawn Mote
+//      91344, // Fashion Coupon
+        139113, 166718, 213026, // 행운의 상자 (K TERA)
+        169886, 169887, 169888, 169889, 169890, 169891, // Locked ???? Strongbox
+
+		//98512, // Pansophic Ash	
+		98513, // Essence
+		98590, // Veilthroch
+
+        //98599, // Devil's Claws
+        //98600, // Dragon Skull
+		
+		88784, // Powerful Transcendent Mask
+		88785, // Keen Transcendent Mask
+		88786, // Bitter Trasncendent Mask
+		88787, // Energetic Transcendent Mask
+		
+		88849, // Powerful Bahaar's Mask
+        88850, // Keen Bahaar's Mask
+        88851, // Bitter Bahaar's Mask
+        88852, // Energetic Bahaar's Mask
+		
+		201656, // Hidden Weapon Sheath
+		202017, // Transparent Mask
+		
+		88827, // Belt Amplifier I
+		88832, // Belt Amplifier II
+		88831, // Circlet Amplifier I
+		88836, // Circlet Amplifier II
+		88829, // Earring Amplifier I
+		88834, // Earring Amplifier II
+		88828, // Necklace Amplifier I
+		88833, // Necklace Amplifier II
+		88830, // Ring Amplifier I
+		88835, // Ring Amplifier II     
+		
+		88837, // Accessory Amplifier Coin
+      
+//      98653, // Supreme Metal
+//      98654, // Springy Draco Limb
+//      98655, // Weight Pendulum
+        98656, // Sacred Mallet
+		
+		88840, // Bahaar's Amplifier Coin
+		88841, // Bahaar's Handwear Amplifier
+		88842 // Bahaar's Footwear Amplifier
+		
+    ]
+}
